76ce5ed59c99567ca4dc74db2447613227b7a8b0
Jerry Zhao <jchao@y7mail.com>
2014-11-22 11:30:41 +1100

    check folder against checksum file created before

--- backup.go
+++ backup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codingsince1985/checksum/md5"
 	"github.com/codingsince1985/util"
 	"os"
@@ -8,15 +9,26 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) == 4 {
+	if len(args) >= 4 {
 		switch args[1] {
 		case "--create":
 			createChecksumFile(args[2], args[3])
+		case "--check":
+			checkChecksumFile(args[2], args[3])
 		}
 	}
 }
 
 func createChecksumFile(root, file string) {
+	folder, err := getChecksum(root)
+	if err != nil {
+		panic(err)
+	}
+
+	folder.Write(file)
+}
+
+func getChecksum(root string) (util.Folder, error) {
 	fileList, err := util.GetFiles(root)
 	if err != nil {
 		panic(err)
@@ -31,6 +43,40 @@ func createChecksumFile(root, file string) {
 		files = append(files, util.File{file[len(root):], md5sum})
 	}
 	folder := util.Folder{root, "md5", files}
+	return folder, nil
+}
 
-	folder.Write(file)
+func checkChecksumFile(root, file string) {
+	folderAfter, err := getChecksum(root)
+	if err != nil {
+		panic(err)
+	}
+
+	folderBefore, err := util.Read(file)
+	if err != nil {
+		panic(err)
+	}
+
+	changed, created, removed := util.Compare(folderBefore.Files, folderAfter.Files)
+
+	if len(changed) > 0 {
+		fmt.Println("Changed")
+		printFiles(changed)
+	}
+
+	if len(created) > 0 {
+		fmt.Println("Created")
+		printFiles(created)
+	}
+
+	if len(removed) > 0 {
+		fmt.Println("Removed")
+		printFiles(removed)
+	}
+}
+
+func printFiles(files []util.File) {
+	for _, file := range files {
+		fmt.Println(file.Name)
+	}
 }
