ed8c77f9b2924e88a748c338082ad734d248c25a
Jerry Zhao <jchao@y7mail.com>
2014-11-25 19:50:30 +1100

    add remote operations

--- backup.go
+++ backup.go
@@ -11,26 +11,35 @@ import (
 func main() {
 	args := os.Args
 	if len(args) >= 4 {
+		root := args[2]
+		if !strings.HasSuffix(args[2], "/") {
+			root = args[2] + "/"
+		}
+
 		switch args[1] {
 		case "--create":
-			createChecksumFile(args[2], args[3])
+			createChecksumFile(root, args[3])
 		case "--check":
 			if len(args) == 4 {
-				checkChecksumFile(args[2], args[3], "")
+				checkChecksumFile(root, args[3], "")
 			} else {
-				checkChecksumFile(args[2], args[3], args[4])
+				remoteRoot := args[4]
+				if !strings.HasSuffix(args[4], "/") {
+					remoteRoot = args[4] + "/"
+				}
+				checkChecksumFile(root, args[3], remoteRoot)
 			}
 		}
 	}
 }
 
-func createChecksumFile(root, file string) {
+func createChecksumFile(root, checksumFile string) {
 	folder, err := getChecksum(root)
 	if err != nil {
 		panic(err)
 	}
 
-	folder.Write(file)
+	folder.Write(checksumFile)
 }
 
 func getChecksum(root string) (util.Folder, error) {
@@ -51,19 +60,64 @@ func getChecksum(root string) (util.Folder, error) {
 	return folder, nil
 }
 
-func checkChecksumFile(root, file, remoteRoot string) {
+func checkChecksumFile(root, checksumFile, remoteRoot string) {
 	folderAfter, err := getChecksum(root)
 	if err != nil {
 		panic(err)
 	}
 
-	folderBefore, err := util.Read(file)
+	folderBefore, err := util.Read(checksumFile)
 	if err != nil {
 		panic(err)
 	}
 
 	changed, created, removed := util.Compare(folderBefore.Files, folderAfter.Files)
+	printComparison(changed, created, removed)
 
+	if len(changed) > 0 || len(created) > 0 || len(removed) > 0 {
+		if remoteRoot != "" {
+			fmt.Print("\nSync changes to remote folder? ")
+			b := make([]byte, 2)
+			if _, err := os.Stdin.Read(b); err != nil {
+				panic(err)
+			}
+
+			if strings.ToUpper(string(b[0])) == "Y" {
+				performUpdate(root, remoteRoot, changed, created, removed)
+			}
+		}
+
+		folderAfter.Write(checksumFile)
+	}
+}
+
+func performUpdate(root, remoteRoot string, changed, created, removed []util.File) {
+	if len(changed) > 0 {
+		fmt.Println("\nChanged")
+		for _, file := range changed {
+			fmt.Println(file.Name)
+			util.CopyRemoteFile(root+file.Name, remoteRoot+file.Name)
+		}
+	}
+
+	if len(created) > 0 {
+		fmt.Println("\nCreated")
+		for _, file := range created {
+			fmt.Println(file.Name)
+			util.CopyRemoteFile(root+file.Name, remoteRoot+file.Name)
+		}
+	}
+
+	if len(removed) > 0 {
+		fmt.Println("\nRemoved")
+		for _, file := range removed {
+			fmt.Println(file.Name)
+			util.DeleteRemoteFile(remoteRoot + file.Name)
+		}
+	}
+}
+
+func printComparison(changed, created, removed []util.File) {
 	if len(changed) > 0 {
 		fmt.Println("\nChanged")
 		printFiles(changed)
@@ -77,20 +131,6 @@ func checkChecksumFile(root, file, remoteRoot string) {
 	if len(removed) > 0 {
 		fmt.Println("\nRemoved")
 		printFiles(removed)
-	}
-
-	if remoteRoot != "" {
-		fmt.Print("\nSync changes to remote folder? ")
-		b := make([]byte, 2)
-		if _, err := os.Stdin.Read(b); err != nil {
-			panic(err)
-		}
-
-		if strings.ToUpper(string(b[0])) == "Y" {
-			fmt.Println("Sync...")
-		} else {
-			fmt.Println("Not syncing...")
-		}
 	}
 }
 
