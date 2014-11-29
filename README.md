afa5198b72f58597ab7c7f222487a4c7edfb4e44
Jerry Zhao <jchao@y7mail.com>
2014-11-29 19:18:50 +1100

    check duplicated files against checksum file

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 Bitrot Killer
-=============
-
+=
 A backup utility that checks checksum to prevent bitrot.
 
-##### Generate checksum file for a directory
+Generate checksum file
+-
 go run backup.go --**create** /home/jerry/shared /home/jerry/shared.json
 
-##### Check directory and optionally sync changes to remote directory
+Check update
+-
 go run backup.go --**check** /home/jerry/shared /home/jerry/shared.json *smb://192.168.8.140/public/sda1/Jerry/shared*
+
+Check duplicated
+-
+go run backup.go --**dedup** /home/jerry/shared.json
--- backup.go
+++ backup.go
@@ -10,30 +10,68 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) >= 4 {
-		root := args[2]
-		if !strings.HasSuffix(args[2], "/") {
-			root = args[2] + "/"
-		}
-
-		switch args[1] {
-		case "--create":
-			createChecksumFile(root, args[3])
-		case "--check":
-			if len(args) == 4 {
-				checkChecksumFile(root, args[3], "")
+	if len(args) >= 3 {
+		switch {
+		case args[1] == "--create" && len(args) == 4:
+			createChecksumFile(args[2], args[3])
+		case args[1] == "--check":
+			if len(args) == 5 {
+				checkChecksumFile(args[2], args[3], args[4])
 			} else {
-				remoteRoot := args[4]
-				if !strings.HasSuffix(args[4], "/") {
-					remoteRoot = args[4] + "/"
-				}
-				checkChecksumFile(root, args[3], remoteRoot)
+				checkChecksumFile(args[2], args[3], "")
 			}
+		case args[1] == "--dedup":
+			checkDuplicated(args[2])
 		}
 	}
 }
 
+func checkDuplicated(checksumFile string) {
+	folder, err := util.Read(checksumFile)
+	if err != nil {
+		panic(err)
+	}
+
+	dupFiles := [][]util.File{}
+	for i, _ := range folder.Files {
+		dupFiles = checkDuplicatedIn(folder.Files[i:], dupFiles)
+	}
+
+	fmt.Println("\nDuplicated files")
+	for _, dupFile := range dupFiles {
+		for _, file := range dupFile {
+			fmt.Println(file.Name)
+		}
+		fmt.Println("")
+	}
+}
+
+func checkDuplicatedIn(files []util.File, dupFiles [][]util.File) [][]util.File {
+	if len(files) >= 2 && files[0].Checksum != "" {
+		testedFile := files[0]
+
+		for _, dupFile := range dupFiles {
+			if testedFile.Checksum == dupFile[0].Checksum {
+				return dupFiles
+			}
+		}
+
+		dupFile := []util.File{testedFile}
+		for _, file := range files[1:] {
+			if testedFile.Checksum == file.Checksum {
+				dupFile = append(dupFile, file)
+			}
+		}
+
+		if len(dupFile) > 1 {
+			dupFiles = append(dupFiles, dupFile)
+		}
+	}
+	return dupFiles
+}
+
 func createChecksumFile(root, checksumFile string) {
+	root = appendSlash(root)
 	folder, err := getChecksum(root)
 	if err != nil {
 		panic(err)
@@ -63,6 +101,9 @@ func getChecksum(root string) (util.Folder, error) {
 }
 
 func checkChecksumFile(root, checksumFile, remoteRoot string) {
+	root = appendSlash(root)
+	remoteRoot = appendSlash(remoteRoot)
+
 	folderAfter, err := getChecksum(root)
 	if err != nil {
 		panic(err)
@@ -99,6 +140,13 @@ func checkChecksumFile(root, checksumFile, remoteRoot string) {
 
 		folderAfter.Write(checksumFile)
 	}
+}
+
+func appendSlash(folder string) string {
+	if !strings.HasSuffix(folder, "/") {
+		return folder + "/"
+	}
+	return folder
 }
 
 func applyChanged(root, remoteRoot string, changedFiles []util.File) {
